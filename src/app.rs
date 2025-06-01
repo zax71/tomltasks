e6c3ba37b7288e756653b5f947cc8bdfdfa8b4bd
Zax71 <67716263+zax71@users.noreply.github.com>
2025-06-01 16:58:44 +0100

    Setup UI for quiz

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,26 @@
+use egui::Align2;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     // Example stuff:
-    label: String,
+    answer: String,
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+    checkbox: bool,
+    show_no_text_dialog: bool,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
+            answer: "".to_owned(),
             value: 2.7,
+            checkbox: false,
+            show_no_text_dialog: false,
         }
     }
 }
@@ -54,37 +60,44 @@ impl eframe::App for TemplateApp {
                     if ui.button("Quit").clicked() {
                         ctx.send_viewport_cmd(egui::ViewportCommand::Close);
                     }
-                });
-                ui.menu_button("Open", |ui| {
-                    if ui.button("Quit").clicked() {
-                        todo!("Implement file opening")
+                    if ui.button("Open").clicked() {
+                        todo!("file opening")
                     }
                 });
+                ui.menu_button("Theme", |ui| {
+                    egui::widgets::global_theme_preference_buttons(ui);
+                });
                 ui.add_space(16.0);
-                egui::widgets::global_theme_preference_buttons(ui);
             });
         });
 
+        if self.show_no_text_dialog {
+            egui::Window::new("Error: No text was entered in the answer")
+                .collapsible(false)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    ui.vertical_centered(|ui| {
+                        if ui.button("Okay").clicked() {
+                            self.show_no_text_dialog = false;
+                        }
+                    })
+                });
+        }
+
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
+            ui.heading("The question");
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
+                ui.label("Answer: ");
+                ui.text_edit_singleline(&mut self.answer);
             });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            if ui.button("Check answer").clicked() {
+                if self.answer.len() == 0 {
+                    self.show_no_text_dialog = true;
+                } else {
+                    println!("Answer is {}", self.answer);
+                }
             }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
