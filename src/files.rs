5752833fb57c124f95fcb9a576e2309274549e0c
Zax71 <67716263+zax71@users.noreply.github.com>
2025-06-04 16:28:19 +0100

    Please Rustfmt

--- src/files.rs
+++ src/files.rs
@@ -3,7 +3,7 @@ use rfd::FileDialog;
 use serde_json;
 use std::path::PathBuf;
 
-use crate::data_structs::{ConfigFile};
+use crate::data_structs::ConfigFile;
 
 /// Requests the user to pick a json file and returns the path to it. Fails if user cancels request
 pub fn pick_json() -> Option<PathBuf> {
@@ -15,8 +15,7 @@ pub fn pick_json() -> Option<PathBuf> {
 
 /// Deserializes the JSON to a Vec<Question>. Fails if JSON is malformed
 pub fn load_str(file: &str) -> Result<ConfigFile> {
-    let questions =
-        serde_json::from_str(file).context("Failed to deserialize questions JSON")?;
+    let questions = serde_json::from_str(file).context("Failed to deserialize questions JSON")?;
 
     Ok(questions)
 }
@@ -51,13 +50,19 @@ mod tests {
     ]
 }"#;
 
-        let serialized_result = ConfigFile { questions: vec![Question {
-            question: "What is 2 + 2?".to_string(),
-            answers: vec!["4".to_string(), "FiSh".to_string(), "four".to_string()],
-        }, Question {
-            question: "What is 1 + 1?".to_string(),
-            answers: vec!["window".to_string(), "2".to_string(), "two".to_string()],
-        }], set_name: "Example set".to_string() } ;
+        let serialized_result = ConfigFile {
+            questions: vec![
+                Question {
+                    question: "What is 2 + 2?".to_string(),
+                    answers: vec!["4".to_string(), "FiSh".to_string(), "four".to_string()],
+                },
+                Question {
+                    question: "What is 1 + 1?".to_string(),
+                    answers: vec!["window".to_string(), "2".to_string(), "two".to_string()],
+                },
+            ],
+            set_name: "Example set".to_string(),
+        };
 
         match load_str(test_json) {
             Ok(deserialized_result) => assert_eq!(deserialized_result, serialized_result),
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
-pub mod files;
 pub mod data_structs;
+pub mod files;
 pub use app::TemplateApp;
